36dc5f442e32331954196b817094faef3ae4f559
Hariom Joshi <goodhariom@gmail.com>
2021-05-26 13:35:42 +0530

    Method overloading examples added

--- src/com/company/cwh_method_overloading.java
+++ src/com/company/cwh_method_overloading.java
@@ -3,12 +3,32 @@ package com.company;
 import java.util.Scanner;
 
 public class cwh_method_overloading {
-    static void telljoke(){
-        System.out.println("Sorry! I have no joke to tell you right now! ");
+    /*static void telljoke(){
+        System.out.println("Sorry! I have no joke to tell you
+         right now! ");
+    }*/
+
+    //method overloading
+
+    static void overloaded(){
+        System.out.println("Here, the first method with the same name is being executed! ");
+    }
+    static void overloaded(String a){
+        System.out.println("Hello " + a + " dost! kaise ho?");
+    }
+    static void overloaded(String a, String b){
+        System.out.println("Hello " + a + " and not " + b);
     }
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        System.out.println("The joke will keep running again and again until you order it not to! ");
+        System.out.println("Enter you name: ");
+        String x = scan.next();
+        String y = scan.next();
+        overloaded();
+        overloaded(x);
+        overloaded(y, x);
+
+        /*System.out.println("The joke will keep running again and again until you order it not to! ");
         System.out.println("/n");
         while(true){
             telljoke();
@@ -21,6 +41,6 @@ public class cwh_method_overloading {
             else{
                 break;
             }
-        }
+        }*/
     }
 }
