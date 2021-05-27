b4d0525b6d21d541cb7f320e7ec4086c071c5c32
Hariom Joshi <goodhariom@gmail.com>
2021-05-27 18:17:58 +0530

    Question 1 and 2 of practice set done

---
+++ src/com/company/cwh_practice_questions_related_to_java.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class cwh_practice_questions_related_to_java {
+
+    // question 1
+    // print the multiplication table of any number x
+
+    /*
+    static void table(int a){
+        for (int i=1; i<=10; i++){
+            int ans = a*i;
+            System.out.printf("%d X %d = %d", a, i, ans);
+            System.out.println("\n");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number of which you want to print the table: ");
+        int num = scan.nextInt();
+        table(num);
+    }
+    */
+
+    // question 2
+    // a program to print the triangle of stars
+
+    /*
+    static void triangle(int a){
+        for(int i=1; i<=a; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the size of one side: ");
+        int num = scan.nextInt();
+        triangle(num);
+    }
+    */
+
+    // Question 3
+    // function to calculate the sum of first n natural numbers
+
+    public static void main(String[] args) {
+
+    }
+}
